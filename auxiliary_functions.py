c0c1a80ded18c84562e7b0851fab16fe16c71938
vdesyatke <nikita.andronov@gmail.com>
2024-03-28 15:10:18 +0400

    Move fetch_spacex into separate file

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -1,5 +1,7 @@
 import os
 from urllib import parse
+from pathlib import Path
+import requests
 
 
 def extract_file_extension_from_path(path):
@@ -15,3 +17,14 @@ def extract_file_basename_from_url(url):
     path = parse.urlsplit(url).path
     quoted_basename = os.path.split(path)[1]
     return parse.unquote(quoted_basename)
+
+
+def download_pic(url, path, params=None):
+    path_to_folder = os.path.dirname(path)
+    Path(path_to_folder).mkdir(parents=True, exist_ok=True)
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    with open(path, 'wb') as file:
+        file.write(response.content)
---
+++ fetch_spacex_images.py
@@ -0,0 +1,57 @@
+from auxiliary_functions import *
+import argparse
+
+
+def create_parser():
+    description = '''Download photos of SpaceX launches'''
+    parser = argparse.ArgumentParser(description=description)
+    h = 'ID of launch'
+    parser.add_argument('-id', '--launch_id', help=h, default='latest')
+    return parser
+
+
+def is_valid_launch_id(id):
+    """Checks if the given id is a valid SpaceX launch id"""
+    url = 'https://api.spacexdata.com/v5/launches'
+    response = requests.get(url)
+    response.raise_for_status()
+    launch_id_list = (launch['id'] for launch in response.json())
+    return id in launch_id_list
+
+
+def return_id_of_latest_photographed_launch():
+    """Returns id of the latest photographed SpaceX launch"""
+    url = 'https://api.spacexdata.com/v5/launches'
+    response = requests.get(url)
+    response.raise_for_status()
+    for launch in response.json()[::-1]:
+        if launch['links']['flickr']['original']:
+            return launch['id']
+
+
+def main():
+    parser = create_parser()
+    launch_id = parser.parse_args().launch_id
+    if launch_id == 'latest':
+        launch_id = return_id_of_latest_photographed_launch()
+
+    if not is_valid_launch_id(launch_id):
+        print(f'{launch_id} is not a valid SpaceX launch id')
+        return None
+
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    images = response.json()['links']['flickr']['original']
+
+    if not images:
+        print(f'No photos provided for the launch {launch_id}')
+    else:
+        for number, image in enumerate(images):
+            ext = extract_file_extension_from_url(image)
+            download_pic(image, f'images/space_{number}{ext}')
+
+
+if __name__ == '__main__':
+    main()
