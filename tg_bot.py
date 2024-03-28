01001ffdab1a6deae10864d9f3edc98c8aa7ff2e
vdesyatke <nikita.andronov@gmail.com>
2024-03-28 20:08:48 +0400

    Publish photo to telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -7,11 +7,13 @@ def main():
     env.read_env()
 
     bot = telegram.Bot(token=env('TG_TOKEN'))
-    print(bot.get_me())
+    # print(bot.get_me())
 
     chat_id = bot.get_updates()[-1].message.chat_id
 
-    bot.send_message(chat_id=chat_id, text="Test message")
+    # bot.send_message(chat_id=chat_id, text="Test message")
+    # bot.send_document(chat_id=chat_id, document=open('images/space_0.jpg', 'rb'))
+    bot.send_photo(chat_id=chat_id, photo=open('images/space_0.jpg', 'rb'))
 
 
 if __name__ == '__main__':
